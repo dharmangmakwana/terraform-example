ed08d1958fdd9143880be374cf9370f7ed3ac677
Dharmang Makwana <dharmang.makwana@panamaxil.com>
2023-05-30 08:16:50 +0530

    Teraform Examples

---
+++ github.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the GitHub Provider
+provider "github" {
+    token = "github_token"
+}
+
+resource "github_repository" "terraform-example" {
+  name        = "terraform-example"
+  description = "My awesome codebase"
+
+  visibility = "public"
+}
