69f44cc5144795d84b20f9c2820db6525638b1b4
Dharmang Makwana <dharmang.makwana@panamaxil.com>
2023-06-02 23:44:34 +0530

    Adding modules

---
+++ resource_reference/ec2_eip_association.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.0.1"
+     }
+  }
+}
+
+provider "aws" {
+  profile    = "${var.profile}"
+  region     = "${var.region}"
+}
+
+resource "aws_eip" "myeip" { 
+}
+
+resource "aws_instance" "web" {
+  ami = "ami-0715c1897453cabd1"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Web Server"
+    CreatedBy = "Dharmang Makwana"
+    Learning = "Terraform"
+  }
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.web.id
+  allocation_id = aws_eip.myeip.id
+}
+
+resource "aws_security_group" "WebSG" {
+  name        = "WebSG"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description      = "SSH from Akshardham Public IP"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["103.85.11.119/32"]
+  }
+}
+
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  security_group_id    = aws_security_group.WebSG.id
+  network_interface_id = aws_instance.web.primary_network_interface_id
+}
