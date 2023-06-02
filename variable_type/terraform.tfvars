bad55e783363e4300ecd44b06ba44f18c748c8f1
Dharmang Makwana <dharmang.makwana@panamaxil.com>
2023-06-03 00:55:02 +0530

    Updating variable block

---
+++ variable_type/terraform.tfvars
@@ -0,0 +1,5 @@
+instance_type = "t2.large"
+ami = "ami-0715c1897453cabd1"
+region="us-east-1"
+profile="default"
+az = ["us-east-1a", "us-east-1b", "us-east-1c"]
---
+++ variable_type/variable.tf
@@ -0,0 +1,29 @@
+variable "region" {
+    default = "us-east-1"
+    type = string
+}
+
+variable "profile" {
+    default = "default"
+    type = string
+}
+
+variable "home_ip" {
+  default = "18.213.236.190/32"
+  type = string
+}
+
+variable "ami" {
+  default = "ami-0715c1897453cabd1"
+  type = string
+}
+
+
+variable "instance_type" {
+  default = "t2.micro"
+  type = string
+}
+
+variable "az" {
+  type = list(string)
+}
