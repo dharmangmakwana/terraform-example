2e6bd54c4696188cac3acc58fcbd713589b93f5b
Dharmang Makwana <dharmang.makwana@panamaxil.com>
2023-06-05 22:57:04 +0530

    Updating variable block

---
+++ datasource/variable.tf
@@ -0,0 +1,33 @@
+variable "region" {
+    default = "us-east-1"
+    type = string
+}
+
+variable "profile" {
+    default = "default"
+    type = string
+}
+
+/*variable "ami" {
+  default = "ami-0715c1897453cabd1"
+  type = string
+}*/
+
+variable "web-count" {
+  type = number
+  default = 2
+}
+variable "instance_type" {
+  default = "t2.micro"
+  type = string
+}
+
+variable "resource_name" {
+  type = list(string)
+  default = [ "WebServer", "TransactionServer", "AdapterServer" ]
+}
+
+variable "deployment_type" {
+  type = string
+  default = "dev"
+}
