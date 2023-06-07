cbd38c8052235b7f31d3ec85cac0b56138c2280d
Dharmang Makwana <dharmang.makwana@panamaxil.com>
2023-06-07 22:12:04 +0530

    Updating variable block

--- remote-provisoner/remote.tf
+++ remote-provisoner/remote.tf
@@ -12,26 +12,35 @@ provider "aws" {
   region  = var.region
 }
 
-# Method 2
 resource "aws_instance" "web" {
   ami           = var.ami
   instance_type = var.instance_type
   key_name      = var.keyname
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = file("./terraform-practice.pem")
-    host        = self.public_ip
-  }
+  # connection {
+  #   type        = "ssh"
+  #   user        = "ec2-user"
+  #   private_key = file("./terraform-practice.pem")
+  #   host        = self.public_ip
+  # }
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum -y install httpd",
-      "sudo systemctl start httpd"
-    ]
-  }
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "sudo yum -y install httpd",
+  #     "sudo systemctl start httpd"
+  #   ]
+  # }
 
   provisioner "local-exec" {
+    when = destroy
+    # when = destroy help us to executing below command before destroying the resource
     command = "echo 'Hello World' >> output.txt" 
   }
 }
+
+/*
+
+Creation Time provisioner - > Creation time provisioner are run only during the creation, not during the updatation or any other life cycle. If the creation-time provisioner failes then the resource is marked as tainted.
+
+Destroy Time Provisioner -> This will run before destroying the resources.
+
+*/
