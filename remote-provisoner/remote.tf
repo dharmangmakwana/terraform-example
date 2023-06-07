e23c149f9651d0d466c9758523303120b0ec25f7
Dharmang Makwana <dharmang.makwana@panamaxil.com>
2023-06-07 21:50:18 +0530

    Updating variable block

---
+++ remote-provisoner/remote.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.0.1"
+    }
+  }
+}
+
+provider "aws" {
+  profile = var.profile
+  region  = var.region
+}
+
+# Method 2
+resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.keyname
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("./terraform-practice.pem")
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum -y install httpd",
+      "sudo systemctl start httpd"
+    ]
+
+  }
+}
