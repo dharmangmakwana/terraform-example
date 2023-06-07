1974c2926024e57a173307cfaa20fccfa4284c97
Dharmang Makwana <dharmang.makwana@panamaxil.com>
2023-06-07 22:19:13 +0530

    Updating variable block

--- remote-provisoner/remote.tf
+++ remote-provisoner/remote.tf
@@ -16,21 +16,23 @@ resource "aws_instance" "web" {
   ami           = var.ami
   instance_type = var.instance_type
   key_name      = var.keyname
-  # connection {
-  #   type        = "ssh"
-  #   user        = "ec2-user"
-  #   private_key = file("./terraform-practice.pem")
-  #   host        = self.public_ip
-  # }
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("./terraform-practice.pem")
+    host        = self.public_ip
+  }
 
-  # provisioner "remote-exec" {
-  #   inline = [
-  #     "sudo yum -y install httpd",
-  #     "sudo systemctl start httpd"
-  #   ]
-  # }
+  provisioner "remote-exec" {
+    on_failure = continue
+    inline = [
+      "sudo yum -y install httpd",
+      "sudo systemctl start httpd"
+    ]
+  }
 
   provisioner "local-exec" {
+    on_failure = continue
     when = destroy
     # when = destroy help us to executing below command before destroying the resource
     command = "echo 'Hello World' >> output.txt" 
